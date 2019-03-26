ec18e6faaaeac3f28867149d1fa3fc2617b7138e
Udo Schneider <Udo.Schneider@homeaddress.de>
2019-03-26 16:06:48 +0100

    Added Output support & refactoring

---
+++ src/ex_hidraw.h
@@ -0,0 +1,18 @@
+//
+// Created by udos on 21.03.19.
+//
+
+#ifndef HIDRAW_EX_HIDRAW_H
+#define HIDRAW_EX_HIDRAW_H
+
+#define INPUT_REPORT_ID 'i'
+#define OUTPUT_ID 'o'
+#define DESCRIPTOR_REPORT_ID 'd'
+#define ERROR_ID 'e'
+
+
+
+#define MAX_REPORT_SIZE 8192
+#define MAX_RESPONSE_SIZE 65536
+
+#endif //HIDRAW_EX_HIDRAW_H
